3f37924bfd1e1ba97bbd9d9e1f8eaf422d5c77a6
Sean O'Toole <seanotoole@mail.com>
2020-01-18 15:39:39 +0000

    Add basic Action template to plug into Gatling
    This adds the required ActionBuilder and Action classes that can
    be plumbed into a Gatling scenario.
    
    This action does not currently finish so leaves the simulation
    constantly running, but it's a start.

---
+++ src/main/scala/io/toolebox/gatlinglambdaextension/action/InvokeAction.scala
@@ -0,0 +1,12 @@
+package io.toolebox.gatlinglambdaextension.action
+
+import io.gatling.core.action.Action
+import io.gatling.core.session.Session
+
+class InvokeAction extends Action {
+  override def name: String = "???"
+
+  override def execute(session: Session): Unit = {
+
+  }
+}
---
+++ src/main/scala/io/toolebox/gatlinglambdaextension/action/InvokeActionBuilder.scala
@@ -0,0 +1,11 @@
+package io.toolebox.gatlinglambdaextension.action
+
+import io.gatling.core.action.Action
+import io.gatling.core.action.builder.ActionBuilder
+import io.gatling.core.structure.ScenarioContext
+
+case class InvokeActionBuilder() extends ActionBuilder {
+  override def build(ctx: ScenarioContext, next: Action): Action = {
+    new InvokeAction()
+  }
+}
