02f29b5fd36e22901ff65eeb9f709e94bda028ed
Sean O'Toole <seanotoole@mail.com>
2020-01-19 18:28:16 +0000

    Update action so it finishes
    Uses the core components stats engine to log the response details, which Gatling
    picks up to finish the simulation. Using the core components clock to do the
    timings.
    
    Adds NameGen extension to use Gatling to generate request names.
    
    Next we're going to need to get access to a preconfigured AWS Lambda client to
    actually perform the lambda invocation. I guess this is where we use the
    protocol to configure the client and then pass extra details such as payload
    into the action.

--- src/main/scala/io/toolebox/gatlinglambdaextension/action/InvokeAction.scala
+++ src/main/scala/io/toolebox/gatlinglambdaextension/action/InvokeAction.scala
@@ -1,12 +1,29 @@
 package io.toolebox.gatlinglambdaextension.action
 
-import io.gatling.core.action.Action
-import io.gatling.core.session.Session
+import io.gatling.commons.stats.OK
+import io.gatling.commons.util.Clock
+import io.gatling.core.CoreComponents
+import io.gatling.core.Predef.Session
+import io.gatling.core.action.{Action, ExitableAction}
+import io.gatling.core.stats.StatsEngine
+import io.gatling.core.util.NameGen
 
-class InvokeAction extends Action {
-  override def name: String = "???"
+class InvokeAction(coreComponents: CoreComponents, val next: Action)
+    extends ExitableAction
+    with NameGen {
 
   override def execute(session: Session): Unit = {
-
+    val start = clock.nowMillis
+    // Do something
+    val end = clock.nowMillis
+    statsEngine.logResponse(session, name, start, end, OK, None, None)
+    next ! session.markAsSucceeded
   }
+
+  override def name: String = genName("invoke")
+
+  override def statsEngine: StatsEngine = coreComponents.statsEngine
+
+  override def clock: Clock = coreComponents.clock
+
 }
