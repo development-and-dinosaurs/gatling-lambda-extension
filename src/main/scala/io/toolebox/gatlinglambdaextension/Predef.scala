b1fe64334e921e508ce6f3a0dacad5814b316b62
Sean O'Toole <seanotoole@mail.com>
2020-01-20 18:20:44 +0000

    Allow protocol to be built in a more conventional way
    This adds a ProtocolBuilder and some more convenience methods to the Predef to allow the
    LambdaProtocol to be built in an easier manner, more like other Gatling protocols.

--- src/main/scala/io/toolebox/gatlinglambdaextension/Predef.scala
+++ src/main/scala/io/toolebox/gatlinglambdaextension/Predef.scala
@@ -1,15 +1,26 @@
 package io.toolebox.gatlinglambdaextension
 
 import io.gatling.core.action.builder.ActionBuilder
+import io.gatling.core.config.GatlingConfiguration
+import io.toolebox.gatlinglambdaextension.protocol.{
+  LambdaProtocol,
+  LambdaProtocolBuilder
+}
 import io.toolebox.gatlinglambdaextension.request.LambdaInvokeBuilder
 
 object Predef {
 
   def lambda(function: String): LambdaInvokeBuilder =
     LambdaInvokeBuilder(function)
+  def lambda(
+      implicit configuration: GatlingConfiguration
+  ): LambdaProtocolBuilder = LambdaProtocolBuilder()
 
   implicit def invokeBuilderToActionBuilder(
       builder: LambdaInvokeBuilder
   ): ActionBuilder = builder.build
 
+  implicit def protocolBuilderToProtocol(
+      builder: LambdaProtocolBuilder
+  ): LambdaProtocol = builder.build
 }
