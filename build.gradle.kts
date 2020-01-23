970b29a2f9250bdd4fa6d044b0720bfb954d8876
Sean O'Toole <seanotoole@mail.com>
2020-01-23 13:25:53 +0000

    Add nexus plugins for automatic publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,11 @@
+import java.time.Duration
 plugins {
     scala
-    `maven-publish`
+    signing
     id("com.diffplug.gradle.spotless") version "3.27.1"
     id("io.toolebox.git-versioner") version "1.3.0"
+    id("io.codearte.nexus-staging") version "0.21.2"
+    id("de.marcphilipp.nexus-publish") version "0.4.0"
 }
 
 group = "io.toolebox"
@@ -23,16 +26,64 @@ spotless {
     }
 }
 
-val sourcesJar by tasks.registering(Jar::class) {
-    archiveClassifier.set("sources")
-    from(sourceSets.main.get().allSource)
+java {
+    withSourcesJar()
+    withJavadocJar()
 }
 
 publishing {
     publications {
-        register("mavenJava", MavenPublication::class) {
+        create<MavenPublication>("mavenJava") {
             from(components["java"])
-            artifact(sourcesJar.get())
+            pom {
+                name.set("Gatling Lambda Extension") 
+                description.set("A Gatling extension for invoking lambda functions.")
+                url.set("https://github.com/toolebox-io/gatling-lambda-extension")
+                licenses {
+                    license {
+                        name.set("The MIT License (MIT)")
+                        url.set("http://opensource.org/licenses/MIT")
+                    }
+                }
+                developers {
+                    developer {
+                        name.set("Sean O'Toole")
+                        email.set("sean@seanotoole.co.uk")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:https://github.com/toolebox-io/gatling-lambda-extension.git")
+                    developerConnection.set("scm:git@github.com:toolebox-io/gatling-lambda-extension.git")
+                    url.set("https://github.com/toolebox-io/gatling-lambda-extension/")
+                }
+            }
         }
+    }
+}
+
+signing {
+    val signingKey: String? by project
+    val signingPassword: String? by project
+    useInMemoryPgpKeys(signingKey, signingPassword)
+    sign(publishing.publications["mavenJava"])
+}
+
+tasks.withType<Sign>().configureEach {
+    onlyIf { project.findProperty("release") == "true" }
+}
+
+nexusStaging {
+	username = project.findProperty("sonatypeUsername") as String
+	password = project.findProperty("sonatypePassword") as String
+    packageGroup = project.group.toString()
+    numberOfRetries = 60
+    delayBetweenRetriesInMillis = 5000
+}
+
+nexusPublishing {
+    connectTimeout.set(Duration.ofMinutes(5))
+    clientTimeout.set(Duration.ofMinutes(5))
+    repositories {
+        sonatype()
     }
 }
