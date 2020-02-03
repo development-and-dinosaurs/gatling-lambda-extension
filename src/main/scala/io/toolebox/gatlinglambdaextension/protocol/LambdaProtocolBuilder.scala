8c0c39be50fd4a6ab7af0f62c1435cc7d77bb641
Sean O'Toole <seanotoole@mail.com>
2020-02-03 20:01:24 +0000

    Add ability to supply invocation timeout override
    The default override is not long enough for long-lived lambda functions.
    
    This change allows clients to override the timeout to be longer (or
    shorter).
    
    The timeout is currently applied to both the apiCallTimeout and the
    apiCallAttemptTimeout, which probably isn't the intended way for it
    to work but worst case scenario I can just provide two new methods
    and deprecate this one if it causes any problems.

--- src/main/scala/io/toolebox/gatlinglambdaextension/protocol/LambdaProtocolBuilder.scala
+++ src/main/scala/io/toolebox/gatlinglambdaextension/protocol/LambdaProtocolBuilder.scala
@@ -1,9 +1,12 @@
 package io.toolebox.gatlinglambdaextension.protocol
 
+import java.time.Duration
+
 case class LambdaProtocolBuilder(
     awsAccessKeyId: Option[String] = None,
     awsSecretAccessKey: Option[String] = None,
     awsSessionToken: Option[String] = None,
+    timeout: Option[Duration] = None,
     endpoint: Option[String] = None,
     region: Option[String] = None
 ) {
@@ -13,6 +16,8 @@ case class LambdaProtocolBuilder(
     copy(awsSecretAccessKey = Some(key))
   def sessionToken(token: String): LambdaProtocolBuilder =
     copy(awsSessionToken = Some(token))
+  def timeout(duration: Duration): LambdaProtocolBuilder =
+    copy(timeout = Some(duration))
   def endpoint(url: String): LambdaProtocolBuilder =
     copy(endpoint = Some(url))
   def region(region: String): LambdaProtocolBuilder =
@@ -22,6 +27,7 @@ case class LambdaProtocolBuilder(
       awsAccessKeyId,
       awsSecretAccessKey,
       awsSessionToken,
+      timeout,
       endpoint,
       region
     )
