00120d4d9c2ec23614532544490f83322ea628a7
Sean O'Toole <seanotoole@mail.com>
2021-02-09 07:58:41 +0000

    Update dependency versions
    This adds a buildSrc project that holds references to all of the versions we
    are using, along with handy comments for where to find the latest version.
    These versions are then referenced in the build script.
    
    Updates all relevant dependencies to the latest versions, including Gradle.

---
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -0,0 +1,9 @@
+object Versions {
+        const val awsSdk = "2.15.78" // https://search.maven.org/artifact/software.amazon.awssdk/bom
+        const val gatling = "3.5.0" // https://search.maven.org/artifact/io.gatling/gatling-core
+        const val nexusPublish = "0.4.0" // https://plugins.gradle.org/plugin/de.marcphilipp.nexus-publish
+        const val nexusStaging = "0.22.0" // https://plugins.gradle.org/plugin/io.codearte.nexus-staging
+        const val scala = "2.13.4" // https://search.maven.org/artifact/org.scala-lang/scala-library
+        const val spotless = "5.9.0" // https://plugins.gradle.org/plugin/com.diffplug.gradle.spotless
+        const val versioner = "1.6.5" // https://plugins.gradle.org/plugin/io.toolebox.git-versioner
+}
