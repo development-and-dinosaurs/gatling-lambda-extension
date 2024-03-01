b407d8747ca4ed76d774f1a89b22a8bf5e82db13
Tyrannoseanus <tyrannoseanus@developmentanddinosaurs.co.uk>
2024-03-01 18:23:39 +0000

    Downgrade Gatling to 3.10.2
    This is to ensure we keep our version aligned with changes we make

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,6 +1,6 @@
 object Versions {
         const val awsSdk = "2.25.0" // https://search.maven.org/artifact/software.amazon.awssdk/bom
-        const val gatling = "3.10.4" // https://central.sonatype.com/artifact/io.gatling/gatling-core
+        const val gatling = "3.10.2" // https://central.sonatype.com/artifact/io.gatling/gatling-core
         const val gitVersioner = "2.0.1" // https://plugins.gradle.org/plugin/uk.co.developmentanddinosaurs.git-versioner
         const val nexusPublish = "2.0.0-rc-2" // https://plugins.gradle.org/plugin/io.github.gradle-nexus.publish-plugin
         const val scala = "2.13.4" // https://search.maven.org/artifact/org.scala-lang/scala-library
