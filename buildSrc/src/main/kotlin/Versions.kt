0b9f5eaa7ddf6e82d77fdd39dd779d176ee93782
Tyrannoseanus <tyrannoseanus@developmentanddinosaurs.co.uk>
2024-03-01 18:42:57 +0000

    Update to Gatling 3.10.3

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,8 +1,8 @@
 object Versions {
-        const val awsSdk = "2.25.0" // https://search.maven.org/artifact/software.amazon.awssdk/bom
-        const val gatling = "3.10.2" // https://central.sonatype.com/artifact/io.gatling/gatling-core
+        const val awsSdk = "2.25.0" // https://central.sonatype.com/artifact/software.amazon.awssdk/bom
+        const val gatling = "3.10.3" // https://central.sonatype.com/artifact/io.gatling/gatling-core
         const val gitVersioner = "2.0.1" // https://plugins.gradle.org/plugin/uk.co.developmentanddinosaurs.git-versioner
         const val nexusPublish = "2.0.0-rc-2" // https://plugins.gradle.org/plugin/io.github.gradle-nexus.publish-plugin
-        const val scala = "2.13.4" // https://search.maven.org/artifact/org.scala-lang/scala-library
+        const val scala = "2.13.4" // https://central.sonatype.com/artifact/org.scala-lang/scala-library
         const val spotless = "6.25.0" // https://plugins.gradle.org/plugin/com.diffplug.gradle.spotless
 }
