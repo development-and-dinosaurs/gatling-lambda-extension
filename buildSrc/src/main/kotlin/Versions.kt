4b161e4308fbff457fe7f4debecf07fbd15e3d80
Tyrannoseanus <tyrannoseanus@developmentanddinosaurs.co.uk>
2024-03-01 18:01:38 +0000

    Update AWS SDK

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,5 +1,5 @@
 object Versions {
-        const val awsSdk = "2.15.78" // https://search.maven.org/artifact/software.amazon.awssdk/bom
+        const val awsSdk = "2.25.0" // https://search.maven.org/artifact/software.amazon.awssdk/bom
         const val gatling = "3.10.4" // https://central.sonatype.com/artifact/io.gatling/gatling-core
         const val nexusPublish = "2.0.0-rc-2" // https://plugins.gradle.org/plugin/io.github.gradle-nexus.publish-plugin
         const val scala = "2.13.4" // https://search.maven.org/artifact/org.scala-lang/scala-library
